c3ef695966a78de39ce3c54a75c4be31c36d7c12
Claudia Beresford <cberesford@pivotal.io>
2016-11-24 11:32:58 +0000

    Setting Gamora pipeline
    Signed-off-by: Alberto Leal <aleal@pivotal.io>

--- ci/reconfigure-pipeline.sh
+++ ci/reconfigure-pipeline.sh
@@ -1,18 +1,18 @@
 #!/bin/bash -ex
 cd $(dirname $0)/..
 
-FLYRC_TARGET=$1
-if [ -z $FLYRC_TARGET ]; then
+flyrc_target=$1
+if [ -z $flyrc_target ]; then
   echo "No target passed, using 'grootfs-ci'"
-  FLYRC_TARGET="grootfs-ci"
+  flyrc_target="grootfs-ci"
 fi
 [ -z $DEBUG ] && DEBUG=0
 
 check_fly_alias_exists() {
   set +e
-  grep $FLYRC_TARGET ~/.flyrc > /dev/null 2>&1
+  grep $flyrc_target ~/.flyrc > /dev/null 2>&1
   if [ $? -ne 0 ]; then
-    echo "Please ensure $FLYRC_TARGET exists in ~/.flyrc and that you have run fly login"
+    echo "Please ensure $flyrc_target exists in ~/.flyrc and that you have run fly login"
     exit 1
   fi
   set -e
@@ -23,12 +23,12 @@ main() {
 
   pipeline_name="grootfs"
   [ $DEBUG -eq 1 ] && pipeline_name="grootfs-test"
-  vars_name="aws"
+  vars_name="gcp"
   [ $DEBUG -eq 1 ] && vars_name="lite"
   vars_from_path="$HOME/workspace/grootfs-ci-secrets/vars/$vars_name.yml"
-  [ $DEBUG -eq 1 ] && FLYRC_TARGET="lite"
+  [ $DEBUG -eq 1 ] && flyrc_target="lite"
 
-  fly --target="$FLYRC_TARGET" set-pipeline --pipeline=$pipeline_name \
+  fly --target="$flyrc_target" set-pipeline --pipeline=$pipeline_name \
     --config=ci/pipeline.yml --load-vars-from=$vars_from_path
 }
 
