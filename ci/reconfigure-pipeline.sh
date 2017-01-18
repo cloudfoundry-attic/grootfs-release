5ba5eef259e8355746032d88156665458dcb54ab
Roberto Jimenez Sanchez <totemteleko@gmail.com>
2017-01-18 15:32:32 +0000

    Fetch concourse vars from lastpass
    [#136419741]
    
    Signed-off-by: Claudia Beresford <cberesford@pivotal.io>

--- ci/reconfigure-pipeline.sh
+++ ci/reconfigure-pipeline.sh
@@ -8,6 +8,25 @@ if [ -z $flyrc_target ]; then
 fi
 [ -z $DEBUG ] && DEBUG=0
 
+main() {
+  check_fly_alias_exists
+	sync_and_login
+
+  pipeline_name="grootfs"
+  [ $DEBUG -eq 1 ] && pipeline_name="grootfs-test"
+  vars_name="gcp"
+  [ $DEBUG -eq 1 ] && vars_name="lite"
+
+  vars_from_lpass="/tmp/$vars_name.yml"
+	trap 'rm "$vars_from_lpass"' EXIT
+
+  lpass show "Shared-Garden/grootfs-concourse-vars-$vars_name" --notes > "$vars_from_lpass"
+  [ $DEBUG -eq 1 ] && flyrc_target="lite"
+
+  fly --target="$flyrc_target" set-pipeline --pipeline=$pipeline_name \
+    --config=ci/pipeline.yml --load-vars-from=$vars_from_lpass
+}
+
 check_fly_alias_exists() {
   set +e
   grep $flyrc_target ~/.flyrc > /dev/null 2>&1
@@ -18,18 +37,15 @@ check_fly_alias_exists() {
   set -e
 }
 
-main() {
-  check_fly_alias_exists
+sync_and_login() {
+	fly -t $flyrc_target sync
 
-  pipeline_name="grootfs"
-  [ $DEBUG -eq 1 ] && pipeline_name="grootfs-test"
-  vars_name="gcp"
-  [ $DEBUG -eq 1 ] && vars_name="lite"
-  vars_from_path="$HOME/workspace/grootfs-ci-secrets/vars/$vars_name.yml"
-  [ $DEBUG -eq 1 ] && flyrc_target="lite"
-
-  fly --target="$flyrc_target" set-pipeline --pipeline=$pipeline_name \
-    --config=ci/pipeline.yml --load-vars-from=$vars_from_path
+	set +e
+  fly -t $flyrc_target containers > /dev/null 2>&1
+  if [[ $? -ne 0 ]]; then
+		fly -t $flyrc_target login
+	fi
+	set -e
 }
 
 main
