4240063bd694747363ff8d105f052e5f04aa4192
Claudia Beresford <cberesford@pivotal.io>
2017-02-02 09:04:17 +0000

    Add task to parse manifests
    [#136419741]
    
    Signed-off-by: Tiago Scolari <tscolari@pivotal.io>

---
+++ ci/scripts/parse-manifests.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+SOURCE_MANIFESTS_PATH=grootfs-ci-secrets/deployments
+MANIFESTS="gamora/concourse.yml grootfs-bench/aws.yml thanos/cf_colocated.yml thanos/diego.yml thanos/datadog-firehose-nozzle.yml"
+
+mkdir -p manifests/gamora
+mkdir -p manifests/grootfs-bench
+mkdir -p manifests/thanos
+
+for manifest in $MANIFESTS
+do
+  full_path="${SOURCE_MANIFESTS_PATH}/${manifest}"
+  ruby -r erb -e "puts ERB.new(File.read('$full_path')).result" > "manifests/${manifest}"
+done
