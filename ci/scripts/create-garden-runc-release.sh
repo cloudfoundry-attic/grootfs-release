e66cb9f76c9d2e66ae059b71e16659f69c5596be
Tiago Scolari <tscolari@pivotal.io>
2017-03-03 10:42:33 +0000

    Remove unused `create-garden-runc-release` task
    Signed-off-by: Roberto Jimenez Sanchez <jszroberto@gmail.com>

--- ci/scripts/create-garden-runc-release.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# vim: set ft=sh
-
-set -e -x
-BUILD_FOLDER=$PWD
-
-cd garden-runc-release
-bosh2 -n create-release --tarball ${BUILD_FOLDER}/bosh-release/garden-runc.tgz --timestamp-version
