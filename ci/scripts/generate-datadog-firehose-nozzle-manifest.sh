06c66491835a77ddc1532109e2d566b6a1a8e12b
Roberto Jimenez Sanchez <jszroberto@gmail.com>
2017-03-03 14:35:02 +0000

    Make cf domain configurable in datadog deployment
    [#141011551]
    
    Signed-off-by: Tiago Scolari <tscolari@pivotal.io>

--- ci/scripts/generate-datadog-firehose-nozzle-manifest.sh
+++ ci/scripts/generate-datadog-firehose-nozzle-manifest.sh
@@ -17,5 +17,6 @@ echo "$CF_SECRETS" > secrets.yml
 
 bosh2 int \
   --var=datadog_api_key=${DATADOG_API_KEY} \
+  --var=system_domain=${CF_SYSTEM_DOMAIN} \
   --vars-file ./secrets.yml \
   grootfs-ci-secrets/deployments/gamora/datadog-firehose-nozzle.yml > manifests/datadog-firehose-nozzle.yml
