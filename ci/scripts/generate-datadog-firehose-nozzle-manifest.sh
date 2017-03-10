c97345884bf0cb2979e6d97ba562242056f95e4f
Claudia Beresford <cberesford@pivotal.io>
2017-03-10 11:08:05 +0000

    Add uaa_certs to datadog-nozzle task
    Signed-off-by: Tiago Scolari <tscolari@pivotal.io>

--- ci/scripts/generate-datadog-firehose-nozzle-manifest.sh
+++ ci/scripts/generate-datadog-firehose-nozzle-manifest.sh
@@ -12,11 +12,14 @@ ensure(){
 
 ensure DATADOG_API_KEY
 ensure CF_SECRETS
+ensure CF_UAA_CERTS
 
 echo "$CF_SECRETS" > secrets.yml
+echo "$CF_UAA_CERTS" > uaa.yml
 
 bosh2 int \
   --var=datadog_api_key=${DATADOG_API_KEY} \
   --var=system_domain=${CF_SYSTEM_DOMAIN} \
   --vars-file ./secrets.yml \
+  --vars-file ./uaa.yml \
   grootfs-ci-secrets/deployments/gamora/datadog-firehose-nozzle.yml > manifests/datadog-firehose-nozzle.yml
