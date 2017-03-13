3644ab903ad202c187f3cd7256c3aaac8eb317bc
Claudia Beresford <cberesford@pivotal.io>
2017-03-13 14:16:53 +0000

    Split benchmark errands over separate deployments
    [#140940105]
    
    Signed-off-by: Roberto Jimenez Sanchez <jszroberto@gmail.com>

--- ci/scripts/generate-grootfs-bench-manifest.sh
+++ ci/scripts/generate-grootfs-bench-manifest.sh
@@ -12,8 +12,9 @@ ensure(){
 
 ensure DATADOG_API_KEY
 ensure DATADOG_APP_KEY
+ensure DRIVER
 
 bosh2 int \
   --var=datadog_api_key=${DATADOG_API_KEY} \
   --var=datadog_app_key=${DATADOG_APP_KEY} \
-  grootfs-ci-secrets/deployments/grootfs-bench/grootfs-bench.yml > manifests/grootfs-bench.yml
+  grootfs-ci-secrets/deployments/grootfs-bench/grootfs-bench-$DRIVER.yml > manifests/grootfs-bench-$DRIVER.yml
