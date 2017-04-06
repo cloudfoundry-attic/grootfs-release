31338482695999c86644d26a5f2d1d1aebc800b6
Tiago Scolari <tscolari@pivotal.io>
2017-04-06 15:34:04 +0100

    Force deployment deletion on ci script
    Signed-off-by: Roberto Jimenez Sanchez <jszroberto@gmail.com>

--- ci/scripts/delete-deployment.sh
+++ ci/scripts/delete-deployment.sh
@@ -6,4 +6,4 @@ bosh2 int --path "/certs/ca_cert" certificates.yml > ca_cert.crt
 echo "Running performance tests..."
 bosh2 -e $BOSH_ENVIRONMENT --ca-cert ca_cert.crt alias-env bosh-director
 bosh2 -e bosh-director --client $BOSH_CLIENT --client-secret $BOSH_CLIENT_SECRET login
-bosh2 -e bosh-director -d $BOSH_DEPLOYMENT delete-deployment
+bosh2 -e bosh-director -d $BOSH_DEPLOYMENT -n delete-deployment
