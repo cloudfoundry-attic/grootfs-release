c12bb1df89f97029ef81006ff03e320998d22ec3
Claudia Beresford <cberesford@pivotal.io>
2017-05-15 16:46:13 +0100

    Concourse task to run Vizzini test suite
    [#145276295]

---
+++ ci/scripts/vizzini.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -x -e
+
+cd vizzini-test-suite
+
+ginkgo -nodes=8 \
+  -skip="{LOCAL}" \
+  -randomizeAllSpecs \
+  -progress \
+  -trace \
+  "$@" \
+  -- \
+  --routable-domain-suffix=${ROUTABLE_DOMAIN}
+  --host-address=${CELL_ADDRESS} \
+  --bbs-address=${BBS_ADDRESS}:8889 \
+  --bbs-client-cert=${BBS_CLIENT_CERT} \
+  --bbs-client-key=${BBS_CLIENT_KEY} \
+  --ssh-address=ssh.${ROUTABLE_DOMAIN} \
+  --ssh-password=${SSH_PASSWORD} \
+
