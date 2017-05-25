0cb4ce0ecbe60db4942b2a09384ba721c697fa3f
Gareth Clay <gclay@pivotal.io>
2017-05-25 16:12:33 +0100

    Include ENV name in tags
    [#145276295]
    
    Signed-off-by: Claudia Beresford <cberesford@pivotal.io>

--- ci/scripts/publish-vizzini-execution.sh
+++ ci/scripts/publish-vizzini-execution.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
+export $IAAS
 export "$DATADOG_API_KEY"
 curl  -X POST -H "Content-type: application/json" \
   -d '{
         "title": "Vizzini errand just finished",
         "text": "YAAAAAY!!",
-        "tags": ["vizzini"]
+        "tags": ["vizzini", "$ENV"]
     }' \
   "https://app.datadoghq.com/api/v1/events?api_key=$DATADOG_API_KEY"
