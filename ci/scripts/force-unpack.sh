0c5f679021bea1bc1d1b64722aba644c553de353
Claudia Beresford <cberesford@pivotal.io>
2017-06-12 09:30:15 +0000

    Fix variable expansion

--- ci/scripts/force-unpack.sh
+++ ci/scripts/force-unpack.sh
@@ -22,7 +22,7 @@ bosh2 int --path "/certs/ca_cert" certificates.yml > ca_cert.crt
 bosh2 -e $BOSH_TARGET --ca-cert ca_cert.crt alias-env bosh-director
 bosh2 -e bosh-director --client $BOSH_CLIENT --client-secret $BOSH_CLIENT_SECRET login
 
-for cell in $(bosh2 -e bosh-director -d cf vms | awk "/$CELL_NAME\// {print $1}")
+for cell in $(bosh2 -e bosh-director -d cf vms | awk '/'"$CELL_NAME"'\// {print $1}')
 do
   bosh2 -e bosh-director -d cf ssh $cell -c "sudo touch /var/vcap/packages/cflinuxfs2/*"
 done
