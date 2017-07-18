224ca972b4260f15cff7af4180b0f97c8e13b61f
Tiago Scolari <tscolari@pivotal.io>
2017-07-18 11:05:53 +0100

    Push canaryapp to each cell cluster
    Signed-off-by: Claudia Beresford <cberesford@pivotal.io>

---
+++ ci/scripts/isolate-canaries.sh
@@ -0,0 +1,32 @@
+#!/bin/bash -ex
+
+ensure(){
+  if [[ "${!1}" == "" ]]
+  then
+    echo "ERROR: ${1} param must be defined"
+    exit 1
+  fi
+}
+
+ensure CF_API_URL
+ensure CF_PASSWORD
+ensure CF_USERNAME
+ensure DATADOG_API_KEY
+ensure DEPLOYMENT_NAME
+
+cf api $CF_API_URL --skip-ssl-validation
+cf auth $CF_USERNAME $CF_PASSWORD
+cf target -o system
+
+spaces=(grootfs shed custom_agent)
+for space in ${spaces[@]}
+do
+  cf create-isolation-segment $space
+  cf enable-org-isolation system $space
+  cf create-space $space
+  cf target -s $space
+  cf set-space-isolation-segment $space $space
+  pushd diegocanaryapp
+    APP_NAME="diego-canary-app-$space" scripts/deploy
+  popd
+done
