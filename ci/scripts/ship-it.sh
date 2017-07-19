06779c2ff8ddfd19463fb9dea2bb710ecf89026e
Gareth Clay <gclay@pivotal.io>
2017-07-19 17:12:36 +0100

    Make ship-it job generic
    * Allow the job to be applied to any release
    * We'll use this for grootfs-diagnostics-release soon
    
    [#149144593]
    
    Signed-off-by: Tiago Scolari <tscolari@pivotal.io>

--- ci/scripts/ship-it.sh
+++ ci/scripts/ship-it.sh
@@ -13,20 +13,20 @@ ensure(){
 
 ensure PRIVATE_YML
 
-VERSION=$(cat ./grootfs-release-version/number)
+VERSION=$(cat ./version/number)
 if [ -z "$VERSION" ]; then
   echo "missing version number"
   exit 1
 fi
 
-echo "$PRIVATE_YML" > grootfs-release-develop/config/private.yml
+echo "$PRIVATE_YML" > dev-release/config/private.yml
 
-cd grootfs-release-develop
+cd dev-release
 
 git config --global user.email "cf-garden+garden-gnome@pivotal.io"
 git config --global user.name "I am Groot CI"
 
-bosh2 -n create-release --final --sha2 --version "$VERSION" --tarball ${BUILD_FOLDER}/final-release/grootfs-${VERSION}.tgz  --name grootfs
+bosh2 -n create-release --final --sha2 --version "$VERSION" --tarball ${BUILD_FOLDER}/final-release/${NAME}-${VERSION}.tgz  --name ${NAME}
 git add -A
 git commit -m "release v${VERSION}"
 
