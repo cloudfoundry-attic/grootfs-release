61367050575644231e586eb7acfafa3d895c815d
Claudia Beresford <cberesford@pivotal.io>
2017-07-26 12:16:29 +0100

    Diego cell with hacked runc release
    [#148995173]

--- ci/scripts/generate-thanos-cf-manifest.sh
+++ ci/scripts/generate-thanos-cf-manifest.sh
@@ -49,6 +49,7 @@ bosh2 int \
   --ops-file grootfs-ci-secrets/deployments/cf-operations/diego-cell-shed.yml \
   --ops-file grootfs-ci-secrets/deployments/cf-operations/diego-cell-custom-agent.yml \
   --ops-file grootfs-ci-secrets/deployments/cf-operations/diego-cell-dstate.yml \
+  --ops-file grootfs-ci-secrets/deployments/cf-operations/diego-runc-hack.yml \
   --ops-file grootfs-ci-secrets/deployments/cf-operations/loopless-bench-errand.yml \
   --ops-file grootfs-ci-secrets/deployments/cf-operations/looped-bench-errand.yml \
   --ops-file git-cf-deployment/operations/test/add-datadog-firehose-nozzle.yml \
