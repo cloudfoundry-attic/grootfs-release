8de3d9872f6de3236de8a2170e97a754133082e3
claudia <cberesford@pivotal.io>
2017-07-27 11:02:17 +0100

    Rename releases for clarity

--- ci/scripts/gats.sh
+++ ci/scripts/gats.sh
@@ -2,6 +2,6 @@
 
 set -e
 
-export GOPATH=$PWD/garden-runc-release
-cd garden-runc-release/src/code.cloudfoundry.org/garden-integration-tests
+export GOPATH=$PWD/garden-runc-release-git
+cd garden-runc-release-git/src/code.cloudfoundry.org/garden-integration-tests
 ginkgo -p -nodes=4 "$@"
